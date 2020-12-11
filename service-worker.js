1193a9b501252e258b35d37bea8463b778478a5b
Z3tsubouou <davgatseren@gmail.com>
2020-12-12 02:18:07 +0800

    Zurag

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bad38458a370c1872d135281292ac303.js"
+  "/precache-manifest.ac94c738437405030071fe4c8efb3d2d.js"
 );
 
 self.addEventListener('message', (event) => {
